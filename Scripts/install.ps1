1359eed0fcfc3f90d7d0d5f2418509a02bb2208c
Petr Tim <petr.timkovski@cayosoft.com>
2026-06-15 17:24:56 +0400

    added install/uninstall scripts

---
+++ Scripts/install.ps1
@@ -0,0 +1,3 @@
+﻿dotnet publish MatrixScreenSaver.csproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -o publish\
+Copy-Item publish\MatrixScreenSaver.exe C:\Windows\System32\MatrixScreenSaver.scr -Force
+Copy-Item publish\WebView2Loader.dll C:\Windows\System32\ -Force
---
+++ Scripts/uninstall.ps1
@@ -0,0 +1,3 @@
+﻿Remove-Item C:\Windows\System32\MatrixScreenSaver.scr -Force
+Remove-Item C:\Windows\System32\WebView2Loader.dll -Force
+Remove-Item $env:TEMP\MatrixScreenSaverWebView2 -Recurse -Force
